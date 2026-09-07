020e029b0eed46bbd64f3aa77d5794928cfb882f
MOSES Y <ymoses546@gmail.com>
2026-09-08 00:01:55 +0530

    add cron job to send health check requests every 14 mins

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,7 @@ import { clerkMiddleware } from "@clerk/express";
 
 import User from "./models/user.model.js";
 import { connectDB } from "./lib/db.js";
+import job from "./lib/cron.js";
 
 
 const app = express();
@@ -41,5 +42,7 @@ if(fs.existsSync(publicDir)) {
 app.listen(PORT, () => {
     connectDB();
     console.log("Server is running on PORT:", PORT);
+
+    if(process.env.NODE_ENV === "production") job.start();
 });
 
