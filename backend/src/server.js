33f73257d59b6fb051feb1ef986bc9810703f299
MOSES Y <ymoses546@gmail.com>
2026-09-08 14:59:14 +0530

    implement authentication routes and middleware for user verification

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,6 +11,7 @@ import { connectDB } from "./lib/db.js";
 import job from "./lib/cron.js";
 
 import clerkWebhook from "./webhooks/clerk.webhook.js";
+import authRoutes from "./routes/auth.route.js";
 
 
 const app = express();
@@ -35,6 +36,8 @@ app.use(clerkMiddleware());
 app.get("/health", (req, res) => {
     res.status(200).json({ message: "Server is healthy" });
 });
+
+app.use("/api/auth", authRoutes);
 
 // if the public directory exists, serve the static files and this is for the production build
 if(fs.existsSync(publicDir)) {
